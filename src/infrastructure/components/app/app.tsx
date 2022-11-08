4bd2504f454d1e1120ec188115d14a745c3d7f4c
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-08 12:42:30 +0100

    Initial commit

---
+++ src/infrastructure/components/app/app.tsx
@@ -0,0 +1,16 @@
+// import { TodoContextProvider } from '../../infrastructure/context/todo/provider';
+// import { Layout } from '../layout/layout';
+// import { AppRoutes } from '../routes/app.routes';
+
+function App() {
+    return (
+        // <Layout>
+        //     <TodoContextProvider>
+        //         <AppRoutes></AppRoutes>
+        //     </TodoContextProvider>
+        // </Layout>
+        <div>App initial</div>
+    );
+}
+
+export default App;
